728046a15e25720eec7a42e6944559e80c49ee85
jhell85 <jhell85@gmail.com>
2022-01-05 16:06:04 -0800

    removed semantic ui from project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React from 'react'
-import 'fomantic-ui-css/semantic.css';
-import { Container} from 'semantic-ui-react';
-import './App.css'
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="ui grid middle aligned background" >
+    <div className="ui grid middle aligned background">
       <div className="row">
         <div className="column">
-      <Container>
-        <div className="ui text container center aligned segment inverted" >
-          <h1 className="ui header">
-            EK Specialties
-          </h1>
-          <h2>Property Management & Heavy Equipment 
-           maintenance and repair</h2> 
-            <h2>for the Portland Oregon area</h2>
-          <h3>(406) 998-9555</h3>
-
+          <div>
+            <div className="ui text container center aligned segment inverted">
+              <h1 className="ui header">EK Specialties</h1>
+              <h2>Property Management & Heavy Equipment maintenance and repair</h2>
+              <h2>for the Portland Oregon area</h2>
+              <h3>(406) 998-9555</h3>
+            </div>
+          </div>
         </div>
-      </Container>
-      </div>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default App;
