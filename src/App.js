e26b165bade7a4fad87de790c6fe969975dbef0f
jhell85 <jhell85@gmail.com>
2022-01-05 17:17:39 -0800

    add 3d gunplay font

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import "./App.css";
+import "./fonts/gunplay-3d.ttf";
 
 const App = () => {
   return (
-    <div className="ui grid middle aligned background">
-      <div className="row">
-        <div className="column">
-          <div>
-            <div className="ui text container center aligned segment inverted">
-              <h1 className="ui header">EK Specialties</h1>
-              <h2>Property Management & Heavy Equipment maintenance and repair</h2>
+    <div>
+      <h1 className="position-absolute top-50 start-50 translate-middle gunplay-font">EK Specialties</h1>
+      {/* <h2>Property Management & Heavy Equipment maintenance and repair</h2>
               <h2>for the Portland Oregon area</h2>
-              <h3>(406) 998-9555</h3>
-            </div>
-          </div>
-        </div>
-      </div>
+            <h3>(406) 998-9555</h3> */}
     </div>
   );
 };
